0e2e85ee5f3bc634b9e3b7d6965a4fdf25f8a1f8
Luca Muroni <luca.muroni@synclab.it>
2025-03-03 17:48:06 +0100

    feat: add email report test for superset

--- docker/pythonpath_dev/superset_config_docker.py
+++ docker/pythonpath_dev/superset_config_docker.py
@@ -26,6 +26,7 @@
 # SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://pguser:pgpwd@some.host/superset"
 # SQLALCHEMY_ECHO = True
 
+from celery.schedules import crontab
 from logging.handlers import RotatingFileHandler
 import logging
 
@@ -37,3 +38,62 @@ handler = RotatingFileHandler(SUPERSET_LOG_DIR, maxBytes=10000000, backupCount=5
 handler.setLevel(LOG_LEVEL)
 handler.setFormatter(logging.Formatter(LOG_FORMAT))
 logging.getLogger().addHandler(handler)
+
+ALERT_REPORTS_NOTIFICATION_DRY_RUN = False
+
+FEATURE_FLAGS = {
+    "ALERT_REPORTS": True
+}
+
+REDIS_HOST = "superset_cache"
+REDIS_PORT = "6379"
+
+class CeleryConfig:
+    broker_url = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    imports = (
+        "superset.sql_lab",
+        "superset.tasks.scheduler",
+    )
+    result_backend = f"redis://{REDIS_HOST}:{REDIS_PORT}/0"
+    worker_prefetch_multiplier = 10
+    task_acks_late = True
+    task_annotations = {
+        "sql_lab.get_sql_results": {
+            "rate_limit": "100/s",
+        },
+    }
+    beat_schedule = {
+        "reports.scheduler": {
+            "task": "reports.scheduler",
+            "schedule": crontab(minute="*", hour="*"),
+        },
+        "reports.prune_log": {
+            "task": "reports.prune_log",
+            "schedule": crontab(minute=0, hour=0),
+        },
+    }
+CELERY_CONFIG = CeleryConfig
+
+SCREENSHOT_LOCATE_WAIT = 100
+SCREENSHOT_LOAD_WAIT = 600
+
+# Email configuration
+SMTP_HOST = "smtp.gmail.com" # change to your host
+SMTP_PORT = 587 # your port, e.g. 587
+SMTP_STARTTLS = True
+SMTP_SSL_SERVER_AUTH = True # If you're using an SMTP server with a valid certificate
+SMTP_SSL = False
+SMTP_USER = "akiba.amvyt@gmail.com" # use the empty string "" if using an unauthenticated SMTP server
+SMTP_PASSWORD = "nbqg uahy syfn xaym" # use the empty string "" if using an unauthenticated SMTP server
+SMTP_MAIL_FROM = "noreply@akiba.amvyt.com"
+EMAIL_REPORTS_SUBJECT_PREFIX = "[Superset] " # optional - overwrites default value in config.py of "[Report] "
+
+# This is for internal use, you can keep http
+WEBDRIVER_BASEURL = "http://superset_app:8088" # When running using docker compose use "http://superset_app:8088'
+# This is the link sent to the recipient. Change to your domain, e.g. https://superset.mydomain.com
+WEBDRIVER_BASEURL_USER_FRIENDLY = "http://localhost:8088"
+
+# Set a minimum interval threshold between executions (for each Alert/Report)
+# Value should be an integer
+# ALERT_MINIMUM_INTERVAL = int(timedelta(minutes=10).total_seconds())
+# REPORT_MINIMUM_INTERVAL = int(timedelta(minutes=5).total_seconds())
